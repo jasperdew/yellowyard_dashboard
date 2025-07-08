deb4eb7e2b422d1235cb106e89947cb163187089
jasperdew <jasper@jasperdewinter.nl>
2025-07-08 20:36:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -779,15 +779,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import numpy as np
-from datetime import datetime, timedelta, date
-import io
-import html
-import re
 
 # Configuratie van de pagina
 st.set_page_config(
